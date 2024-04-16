61dd3f42a0ddf95739cbb161e5307138def47445
rahulonit <rahulonit@gmail.com>
2024-04-16 13:26:18 +0530

    first push

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import AuthStack from './src/navigation/AuthStack';
-import AppStack from './src/navigation/AppStack';
-
-function App() {
+export default function App() {
   return (
-    <NavigationContainer>
-      {/* <AppStack /> */}
-      <AuthStack />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
